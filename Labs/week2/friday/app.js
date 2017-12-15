afad83de91e3166d3f572e6ec609e3b2d4f8e4d4
Jorge Rodriguez <j_rod15@msn.com>
2017-12-15 16:34:26 -0600

    HELP NEEDED!

---
+++ Labs/week2/friday/app.js
@@ -0,0 +1,18 @@
+// $(document).ready(function({
+// });
+
+  // function addListItem() {
+  //   var text = $("#new-text").val();
+  //   alert(text);
+  // }
+
+  // $(function() {
+  //   $("#add").on('click', addListItem);
+  //
+  //   function addListItem() {
+  //     var text = $("#new-text").val();
+  //     alert(text);
+  //   }
+  // });
+
+  $("h1").css("color", "red");

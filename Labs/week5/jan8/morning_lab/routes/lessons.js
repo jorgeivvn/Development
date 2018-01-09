bd5bc57dca46da4c1d62735711f6b326ef689477
Jorge Rodriguez <j_rod15@msn.com>
2018-01-08 23:09:47 -0600

    3rd commit- Added sneakers show ejs

--- Labs/week5/jan8/morning_lab/routes/lessons.js
+++ Labs/week5/jan8/morning_lab/routes/lessons.js
@@ -13,6 +13,12 @@ router.get('/sneakers', function(req, res){
   res.render('sneakers/index', {sneakersArray: sneakersArray});
 });
 
-
+router.get('/sneakers/:id', function(req, res){
+	for (let i = 0; i < sneakersArray.length; i++) {
+		if (sneakersArray[i].id == req.params.id) {
+	      res.render('sneakers/show', {sneakers: sneakersArray[i]});
+		}
+	}
+});
 
 module.exports = router;

94b3a2b5aeff2c0e0f95ed6492b383aee267d6ba
Jorge Rodriguez <j_rod15@msn.com>
2018-01-10 21:17:50 -0600

    Initial commit- File set up

---
+++ Labs/week5/jan10/mongomongooselab/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require ('morgan');
+const path = require('path');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.set('view engine', 'ejs');
+
+var Todo = require('./models/todo');
+
+var today = new Todo({
+  task: "Go to GA class today!",
+  description: "Create a this TODO list"
+});
+
+//create get route
+app.get('/save', function(req, res){
+  today.save(function(err, newTodo) {
+    if(err) {return console.log(err);}
+    console.log("saved todo: ", newTodo);
+  });
+});
+
+app.get('*', function(req, res) {
+  res.send('On the home page!')
+});
+// app.get('/models/todo', function (req, res) {
+//   db.Todo.find(function(err, todo) {
+//     if (err) {
+//       console.log("index error: " + err);
+//     }
+//     res.json(todo);
+//   });
+// });
+
+app.listen(3000, () => {
+  console.log('YOOOOO Whats good?');
+  });

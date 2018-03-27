9a3c97d1ff3d83fb8561a208b65214ccff0b9d7d
Jorge Rodriguez <j_rod15@msn.com>
2018-03-26 23:16:16 -0500

    More javascript practice

--- Personal_Projects/javascript_practice/javascript.js
+++ Personal_Projects/javascript_practice/javascript.js
@@ -46,3 +46,36 @@ console.log(myName[1]);
 console.log(myName[2]);
 console.log(myName[3]);
 console.log(myName[4]);
+
+var codeWord1 = "are";
+var codeWord2 = "tubas";
+var codeWord3 = "unsafe";
+var codeWord4 = "?!";
+
+console.log(codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1]);
+
+//When slicing, the first number in the parantheses is the number of the character that begins the slice, and the second number in the parantheses is the number of the character AFTER the last character in the slice.
+console.log(myName.slice(1,4));
+
+var longString = "My long string is long";
+
+console.log(longString.slice(2, 17));
+
+console.log(longString.slice(7));
+
+var myHometown = "Rio Rico, AZ";
+
+console.log(myHometown.toUpperCase());
+
+console.log(myHometown.toLowerCase());
+
+
+var greeting = "hELlo THERE, hOW ARE yOu doINg?";
+var lowerCase = greeting.toLowerCase();
+var capitalH = lowerCase[0].toUpperCase();
+var restOfWord = lowerCase.slice(1);
+
+console.log(capitalH + restOfWord);
+
+//Another way to convert 'greeting' to Hello there, how are you doing?
+console.log(greeting[0].toUpperCase() + greeting.slice(1).toLowerCase());

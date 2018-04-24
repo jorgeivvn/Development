cd5127419722af3e7e50b39c3e560c00a748f6b3
Jorge Rodriguez <j_rod15@msn.com>
2018-04-24 17:06:14 -0500

    while loops

--- Personal_Projects/javascript_practice/script.js
+++ Personal_Projects/javascript_practice/script.js
@@ -55,3 +55,11 @@ if (otherName.toLowerCase() === "jorge".toLowerCase()) {
 } else {
   console.log("Hello stranger!")
 };
+
+//Counting sheep with a While Loop
+var sheepCounted = 0;
+while (sheepCounted < 10) {
+  console.log("I have counted " + sheepCounted + " sheep!");
+    sheepCounted++;
+}
+console.log("Zzzzzzzzzz");

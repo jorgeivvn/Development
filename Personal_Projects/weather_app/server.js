57efb56fdb5a6cea23b7a48836c8e603870d959f
Jorge Rodriguez <j_rod15@msn.com>
2018-05-01 19:44:27 -0500

    weather app- first commit

---
+++ Personal_Projects/weather_app/server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var url = 'http://api.openweathermap.org/data/2.5/weather?id=4058076&APPID=8e21efc86f5a2d4198cf1d4e4cbaead0';
+
+var server = http.createServer(function(request,response){
+  var request = require('request');
+  request(url, function(err,res, body){
+    var data = JSON.parse(body);
+    var tempConversion = data['main'].temp*(9/5)-459.67;
+    var currentTemperature = Math.floor(tempConversion);
+    console.log(currentTemperature);
+    response.write("<html><body><div id='container'>");
+    response.write("<h1>"+ 'City Name - : ' + data['name'] + '<br>' + "</h1>");
+    response.write("<h2>" + 'Temperature - : ' + currentTemperature + '<br>' + '</h2');
+    response.write("<h2>" + 'Sunset Time -: ' + new Date(data.sys['sunset']+1000)+'</h2');
+    response.write("</div></body></html>");
+    response.end();
+  })
+}).listen(8081, () => {
+  console.log("Listening!");
+});

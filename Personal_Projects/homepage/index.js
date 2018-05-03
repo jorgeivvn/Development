bce4f27c1b1ea1c62643e878b29deeb13fa424b2
Jorge Rodriguez <j_rod15@msn.com>
2018-05-02 22:12:57 -0500

    3rd commit - to do list functional

--- Personal_Projects/homepage/index.js
+++ Personal_Projects/homepage/index.js
@@ -3,15 +3,26 @@ var app = express();
 var bodyParser = require('body-parser');
 var PORT = Number(process.env.PORT || 5000);
 
+
+//Middleware
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var tasks = ['go to gym', 'prepare for Oracle interview'];
+app.post('/addtask', function (req, res) {
+  var newTask = req.body.newtask;
+  tasks.push(newTask);
+  res.redirect('/');
+});
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {tasks: tasks});
 });
 
-app.post('/addtask', function (req, res) {
-  res.render('index')
-});
+var complete = ['take dog for a walk'];
+app.post('/deletetask', function(req, res) {
+  var completeTask = req.body.check;
+})
 
 app.listen(PORT, function () {
   console.log('Listening on port 5000!');

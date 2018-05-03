d96192e8b17888df397e6917e07ffc975fab4deb
Jorge Rodriguez <j_rod15@msn.com>
2018-05-02 21:23:01 -0500

    2nd commit -added index.ejs file

--- Personal_Projects/homepage/index.js
+++ Personal_Projects/homepage/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var PORT = Number(process.env.PORT || 5000);
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello Oracle World');
+  res.render('index');
+});
+
+app.post('/addtask', function (req, res) {
+  res.render('index')
 });
 
 app.listen(PORT, function () {

5e3dd4e47bdc0fd4fc980026fb273f4f922d5a87
Jorge Rodriguez <j_rod15@msn.com>
2018-05-11 14:54:50 -0700

    4th commit - added parameter

--- Personal_Projects/twitter_bot/app.js
+++ Personal_Projects/twitter_bot/app.js
@@ -2,3 +2,15 @@ var Twitter = require('twitter');
 var config = require('./config.js');
 
 var T = new Twitter(config);
+
+
+//Two parts to favoriting a tweet:
+//1. Search for tweets that you want to favorite
+//2. Select one, and favorite it.
+
+var params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}

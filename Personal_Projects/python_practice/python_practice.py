4d881fc2d3c2b28f52e40ecee40e5f5ec812e31f
Jorge Rodriguez <j_rod15@msn.com>
2018-05-30 20:48:27 -0500

    1st commit - python practice

---
+++ Personal_Projects/python_practice/python_practice.py
@@ -0,0 +1,31 @@
+print("Hello World")
+
+print(8 * 3.57)
+
+print(10*365)
+print(20 + 3650)
+
+fred = 20
+bob = 20
+
+jorge = fred * bob * 2
+
+print(jorge)
+
+fred = 400
+print(fred)
+
+found_coins = 20
+magic_coins = 10
+stolen_coins = 3
+
+coins_at_end_of_year = found_coins + magic_coins * 365 - stolen_coins * 52
+print(coins_at_end_of_year)
+
+terminator = "Arnold Shcwarzenegger is The Terminator"
+print(terminator)
+
+robocop = '''boom
+boom baby
+shakalaka'''
+print(robocop)
